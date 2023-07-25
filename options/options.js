6bd9eaa37fdd522f2525e418e768e3ce4e1d8ab3
Crowdbotics <team@crowdbotics.com>
2023-07-25 01:23:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-mouse-42400",
+  url: "https://lively-mouse-42400.botics.co",
+  api: "https://lively-mouse-42400.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
